f8038c2a3e87854f158953d13dd5b74ff6e64197
loranK <loran.kalisch@elbitsystems.com>
2017-02-09 10:49:33 +0200

    com

--- src/app/lesson1/lesson1.component.ts
+++ src/app/lesson1/lesson1.component.ts
@@ -1,40 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostBinding, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-lesson1',
   templateUrl: './lesson1.component.html',
-  styleUrls: ['./lesson1.component.css'],
-  host: {
-    '(document:keydown)': 'handleKeyboardEvents($event)',
-    '(document:keyup)': 'keyup($event)'
-  },
+  styleUrls: ['./lesson1.component.css']
 
 })
 export class Lesson1Component implements OnInit {
   title = 'app works!';
-  lista:string[]=['1','2','3','4'];
+  lista:string[]=['1','2','3','4','5'];
   todoss:string[]=['aaaaa','b','cs'];
   textValue:string;
   headerText="Click item to change the header"
-  key;
 
- //set 'key' when key was down
-  handleKeyboardEvents(event: KeyboardEvent) {
-    this.key = event.which || event.keyCode;
-  }
 
-  //set 'key' when key is up (to reset key value after key is down)
-  keyup(event: KeyboardEvent) {
-    this.key = null;
-  }
   addNewItem(){
     this.lista.push(this.textValue)
   }
 
 
-  liclick(litext,i){
-
-   if (this.key==16 ){
+  liclick(litext,i,event){
+   if (event.shiftKey ){
       this.lista.splice(i,1);
     }
     else{
